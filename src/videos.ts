b4bd2f29da91fee39c2e136756eae1f234bba8e4
Mobin Askari <132358316+MobinAskari@users.noreply.github.com>
2024-07-07 12:22:11 +0330

    V 0.1 (#1)
    * apply typescript configurations
    
    * add scripts + packages
    
    * create videos schemas and respective tests
    
    * remove pnpm due to "npm link issues"
    
    * change schemas.spec to videos.spec
    
    * remove redundant type
    
    * 0.1.0
    
    * fix test and package name

---
+++ src/videos.ts
@@ -0,0 +1,28 @@
+import { z } from "zod"
+
+export const videoSchema = z
+	.object({
+		name: z.string().trim().min(1),
+		original_video_url: z.string().url().trim(),
+		markdown_description: z.string().optional(),
+		is_leaked: z.boolean().default(false).optional(),
+		bucket_thumbnail_id: z.number().optional(),
+		bucket_leaked_video_id: z.number().optional(),
+		createdAt: z.date().optional(),
+		updatedAt: z.date().optional()
+	})
+	.strict()
+
+const videoWithIDSchema = videoSchema.merge(
+	z.object({ id: z.number() })
+)
+
+export const partialVideoSchema = videoSchema.partial()
+
+export type Video = z.infer<typeof videoWithIDSchema>
+
+export type CreateVideoDto = z.infer<typeof videoSchema>
+
+export type UpdateVideoDto = z.infer<
+	typeof partialVideoSchema
+>
---
+++ tests/videos.spec.ts
@@ -0,0 +1,34 @@
+import { describe, expect, it } from "vitest"
+import {
+	videoSchema,
+	type CreateVideoDto,
+	type UpdateVideoDto
+} from "../src/videos.js"
+
+describe("videoSchema", () => {
+	it("should parse correctly", () => {
+		const data: CreateVideoDto = {
+			name: "hello",
+			original_video_url: "http://t.me",
+			bucket_leaked_video_id: 1,
+			bucket_thumbnail_id: 1,
+			createdAt: new Date(),
+			updatedAt: new Date(),
+			is_leaked: false,
+			markdown_description: "# Hi"
+		}
+
+		const parseResult = videoSchema.parse(data)
+
+		expect(parseResult).toEqual(data)
+	})
+
+	it("should throw if the provided data is invalid", () => {
+		const data: UpdateVideoDto = {
+			name: "hello",
+			original_video_url: "http://"
+		}
+
+		expect(() => videoSchema.parse(data)).toThrowError()
+	})
+})
